3cce91b86446d82f65a98a78d070542080685ef3
Théodore Riant <theodore.riant@protonmail.com>
2026-04-03 11:25:29 +0200

    chore: publier @theodoreriant/prado-ui@0.1.0 sur npm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   modules: [
-    '@prado/ui/nuxt',
+    '@theodoreriant/prado-ui/nuxt',
     '@nuxtjs/supabase',
     '@nuxtjs/prismic',
   ],
